c06883b44545a98d586b548aa963a321e234623f
diegopolverelli <diegopolverelli@hotmail.com>
2023-05-28 17:44:54 -0300

    pase

--- clase 20 OWASP Documentacion/pruebas/ch-backend-main/src/middlewares/logger.middleware.js
+++
@@ -1,6 +0,0 @@
-import { logger } from "../utils/logger.js";
-
-export const addLogger = (req, res, next) => {
-  req.logger = logger;
-  next();
-};
--- clase 20 OWASP Documentacion/pruebas/ch-backend-main/src/public/js/forgotPassword.js
+++
@@ -1,1 +0,0 @@
-console.log("forgotPassword.js ok!");
